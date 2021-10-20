e65cf09bc3ed59a52b7b9b1347e8911c42103e74
John Cassidy <jpcassidy@outlook.com>
2021-10-20 14:49:01 -0400

    Shopping.API create

---
+++ Shopping/Shopping.API/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Shopping.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.API.Controllers {
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase {
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger) => _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+
+        [HttpGet]
+        public async Task<IEnumerable<Product>> Get() {
+            var rng = new Range();
+            return Enumerable.Range(1, 5).Select(index => new Product {
+                Name = "asd"
+            }).ToArray();
+        }
+    }
+}
